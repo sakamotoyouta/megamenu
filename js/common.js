77a3604087bd220a25d5c9522de78c4778b29395
坂本龍太 <sakamotoryuuta@sakamotoryuuta-no-MacBook-Pro.local>
2016-12-12 20:17:45 +0900

    ファイルわけ

---
+++ js/common.js
@@ -0,0 +1,46 @@
+ $(function(){
+	 var megaMenu = {
+		 init: function(){
+			 this.setParam();
+			 this.bindEvent();
+		 },
+		 setParam: function(){
+			 this.menuItem = $('.mega-menu__group');
+			 this.menuItemHead = $('.mega-menu__group__head');
+			 this.menuItemHeadLink = $('.mega-menu__group__head__link');
+			 this.menuItemItem = $('.mega-menu__group__wrapper');
+			 this.megaMenu = $('.mega-menu');
+			 this.timer = 0;
+		 },
+		 bindEvent: function(){
+			 var self = this;
+			 this.megaMenu.mousemove(function(e){
+				 clearTimeout(self.timer);
+				 self.timer = setTimeout(function(){
+					 if($(e.target).is(self.menuItemHead) || $(e.target).is(self.menuItemHeadLink)){
+						 self.closeMenu();
+						 if($(e.target).is(self.menuItemHeadLink)){
+							 self.openMenu($(e.target.parentElement));
+						 }else{
+							 self.openMenu($(e.target));
+						 }
+					 }
+				 },45);
+			 });
+			 this.megaMenu.mouseleave(function(){
+				 setTimeout(function(){
+					 self.closeMenu();
+				 },55);
+			 });
+		 },
+		 openMenu: function(selfElement){
+			 selfElement.addClass('mega-menu__group__head--on');
+			 selfElement.next(this.menuItemItem).show();
+		 },
+		 closeMenu: function(){
+			 this.menuItemHead.removeClass('mega-menu__group__head--on');
+			 this.menuItemItem.hide();
+		 }
+	 };
+	 megaMenu.init();
+ });
